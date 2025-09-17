53dc92febde11cec3b4b71e2c96bc62446ed37e8
Atish-IITM <23f2001258@ds.study.iitm.ac.in>
2025-09-17 15:34:02 +0530

    feat: Added main file and initial exponent and factorial modules.

---
+++ factorial.py
@@ -0,0 +1,12 @@
+# factorial.py
+def factorial(n):
+    """Calculates the factorial of a non_negative integer."""
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    
+    result = 1
+    for i in range(n, 0, -1):
+        result *= i 
+    return result 
+
+print(factorial(5))
